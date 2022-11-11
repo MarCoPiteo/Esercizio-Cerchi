a05debe55069cebf08acb6b81e24838ea0451e36
MarCoPiteo <31marcopiteo31@gmail.com>
2022-11-11 13:58:24 +0100

    Aggiunta random

--- static/js/Cerchi.js
+++ static/js/Cerchi.js
@@ -1,7 +1,20 @@
+let width = 400
+let height = 400
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(width, height);
+  background("green");
 }
 
 function draw() {
-  background(green);
+  let x = random(0, width)
+  let y = random(0, height)
+  let diameter = random(30, 90)
+
+  let red = random(0, 255)
+  let green = random(0, 255)
+  let black = random(0,255)
+  let alpha = random(0, 255)
+
+  
 }
